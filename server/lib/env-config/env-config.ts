dba82793a8d96ef668a308f3bb89241ce187d42c
nbarrett <nick.barrett36@me.com>
2025-03-04 23:42:05 +0000

    - For Server, remove fallback and fix port to 5001

--- server/lib/env-config/env-config.ts
+++ server/lib/env-config/env-config.ts
@@ -43,7 +43,7 @@ export const envConfig = {
     uri: validatedEnvironmentVariable("MONGODB_URI"),
   },
   server: {
-    listenPort: process.env.PORT || 5001,
+    listenPort: 5001,
     staticUrl: "/",
     uploadDir: "/tmp/uploads",
   },
