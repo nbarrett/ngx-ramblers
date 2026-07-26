nbarrett/ngx-ramblers

commit 7a4cd7b82ac66522c7d712afc7df3056188ab622

- Fixed broken forgot password flow - Migrated all backend mongoose code to typescript - Added types for bcryptjs, jsonwebtoken, passport, passport-jwt

server/lib/brevo/brevo-routes.ts
@@ -25,8 +25,8 @@ import * as authConfig from "../auth/auth-config";
 
 const router = express.Router();
 
-router.get("/folders", authConfig.authenticate(), folders);
-router.get("/contacts", authConfig.authenticate(), contacts);
+router.get("/folders", folders);
+router.get("/contacts", contacts);
 router.get("/contacts/list", contactsInList);
 router.post("/contacts/delete", authConfig.authenticate(), contactsDelete);
 router.post("/contacts/create", authConfig.authenticate(), contactsCreate);
@@ -38,13 +38,14 @@ router.get("/contacts/attributes", authConfig.authenticate(), attributes);
 router.get("/senders", authConfig.authenticate(), senders);
 router.post("/senders/create", authConfig.authenticate(), createSender);
 router.post("/transactional/send", authConfig.authenticate(), sendTransactionalMail);
+router.post("/transactional/forgot-password-send", sendTransactionalMail);
 router.post("/campaign/create", authConfig.authenticate(), createCampaign);
 router.post("/campaign/send", authConfig.authenticate(), sendCampaign);
 router.get("/segments", authConfig.authenticate(), querySegments);
-router.post("/templates", authConfig.authenticate(), queryTemplates);
-router.get("/lists", authConfig.authenticate(), lists);
+router.post("/templates", queryTemplates);
+router.get("/lists", lists);
 router.delete("/lists/delete", authConfig.authenticate(), listDelete);
 router.post("/lists/create", authConfig.authenticate(), listCreate);
 router.post("/lists/update", authConfig.authenticate(), listUpdate);
-router.get("/account", authConfig.authenticate(), queryAccount);
+router.get("/account", queryAccount);
 export const brevoRoutes = router;

projects/ngx-ramblers/src/app/auth/auth.interceptor.ts
@@ -14,7 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
   private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   private logger: Logger;
 
-  constructor(public authService: AuthService, private loggerFactory: LoggerFactory) {
+  constructor(public authService: AuthService, loggerFactory: LoggerFactory) {
     this.logger = loggerFactory.createLogger(AuthInterceptor, NgxLoggerLevel.OFF);
   }
 

projects/ngx-ramblers/src/app/auth/auth.service.ts
@@ -18,18 +18,18 @@ export class AuthService {
   private BASE_URL = "/api/database/auth";
   private readonly AUTH_TOKEN = "AUTH_TOKEN";
   private readonly REFRESH_TOKEN = "REFRESH_TOKEN";
-  private authPayload;
+  private authPayload: {};
   private logger: Logger;
   private authResponseSubject = new Subject<LoginResponse>();
 
   constructor(private http: HttpClient,
-              private loggerFactory: LoggerFactory,
+              loggerFactory: LoggerFactory,
               private broadcastService: BroadcastService<any>,
               private siteEditService: SiteEditService) {
     this.logger = loggerFactory.createLogger(AuthService, NgxLoggerLevel.OFF);
   }
 
-  login(userName, password): Promise<LoginResponse> {
+  login(userName: string, password: string): Promise<LoginResponse> {
     const url = `${this.BASE_URL}/login`;
     this.logger.debug("logging in", userName, "via", url);
     const body = {userName, password};
@@ -44,7 +44,7 @@ export class AuthService {
     return this.performAuthPost(url, body, type);
   }
 
-  resetPassword(userName, newPassword, newPasswordConfirm): Promise<LoginResponse> {
+  resetPassword(userName: string, newPassword: string, newPasswordConfirm: string): Promise<LoginResponse> {
     const url = `${this.BASE_URL}/reset-password`;
     const type = "resetting password";
     this.logger.debug(type + " for", userName, "via", url);
@@ -99,16 +99,6 @@ export class AuthService {
     }).pipe(
       tap((tokens: AuthTokens) => {
         this.storeAuthToken(tokens.auth);
-      })
-    );
-  }
-
-  auditMemberLogin(userName: string, loginResponse: LoginResponse) {
-    const url = `${this.BASE_URL}/audit-member-login`;
-    this.logger.debug("calling", url);
-    return this.http.post<any>(url, {userName, loginResponse}).pipe(
-      tap((response) => {
-        this.logger.debug("auditMemberLogin:response:", response);
       })
     );
   }

projects/ngx-ramblers/src/app/models/auth-data.model.ts
@@ -1,8 +1,14 @@
-import { LoginResponse, MemberCookie } from "./member.model";
+import { LoginResponse, Member, MemberCookie } from "./member.model";
+import { Identifiable } from "./api-response.model";
 
 export interface AuthCredentials {
   userName: string;
   password: string;
+}
+
+export interface RefreshToken extends Identifiable {
+  refreshToken: string;
+  memberPayload: Member;
 }
 
 export interface AuthResponse {
@@ -11,6 +17,7 @@ export interface AuthResponse {
     refresh: string;
   };
   loginResponse: LoginResponse;
+  error?: any;
 }
 
 export interface AuthPayload extends Partial<MemberCookie> {

projects/ngx-ramblers/src/app/models/member.model.ts
@@ -176,8 +176,7 @@ export enum MemberAction {
   skipped = "skipped"
 }
 
-export interface MemberAuthAudit {
-  id: string;
+export interface MemberAuthAudit extends Identifiable {
   userName: string;
   loginTime: number;
   loginResponse: LoginResponse;
@@ -193,7 +192,7 @@ export interface Auditable extends Identifiable {
 
 export interface LoginResponse {
   userName?: string;
-  member?: object;
+  member?: MemberCookie | Member;
   alertMessage?: string;
   showResetPassword?: boolean;
   memberLoggedIn?: boolean;

projects/ngx-ramblers/src/app/pages/admin/data-population.service.ts
@@ -57,8 +57,14 @@ export class DataPopulationService {
         category: "admin"
       },
       {
-        name: "mail-settings-list-settings",
+        name: "mail-settings-account-help",
         text: "* This page allows Lists to be created, renamed or deleted in Brevo.\n* Depending on the type of message you are sending out,  you might want to send to a list that contains all members (e.g. periodic newsletters). However you may sometimes want to send messages to a specific list of members in your group (e.g. Walk Leaders or Committee Members). If this is the case, you can create lists and add the members to the lists.\n* There is no limit on the number of lists that can be created in Brevo and when emails are sent out, the the target audience is selected by choosing a list to send to.",
+        category: "admin"
+      },
+      {
+        name: "mail-settings-account-settings",
+        text: "* The fields below are readonly and reflect only what was previously input into Brevo when [this account](https://app.brevo.com/profile/information) was originally setup.\n" +
+          "* When campaign emails are sent out to members, the footer on the emails may contain some of these fields for identification purposes.",
         category: "admin"
       },
       {

projects/ngx-ramblers/src/app/pages/login/forgot-password-modal/forgot-password-modal.component.ts
@@ -122,7 +122,7 @@ export class ForgotPasswordModalComponent implements OnInit, OnDestroy {
     });
     this.subscriptions.push(this.authService.authResponse().subscribe(async (loginResponse) => {
       this.logger.debug("subscribe:forgot password", loginResponse);
-      if (loginResponse.member) {
+      if (loginResponse?.member) {
         this.forgottenPasswordMember = loginResponse.member as Member;
         this.sendForgottenPasswordEmailToMember();
       } else {
@@ -166,7 +166,7 @@ export class ForgotPasswordModalComponent implements OnInit, OnDestroy {
     });
     this.logger.info("sendForgottenPasswordEmailToMember:emailRequest:", this.emailRequest);
     return Promise.resolve(this.notify.success("Sending forgotten password email"))
-      .then(() => this.mailService.sendTransactionalMessage(this.emailRequest))
+      .then(() => this.mailService.sendForgotPasswordMessage(this.emailRequest))
       .then(() => this.finalMessage())
       .then(() => this.notify.clearBusy())
       .catch((error) => this.handleSendError(error));

projects/ngx-ramblers/src/app/services/lazy-loading-metadata.service.ts
@@ -40,25 +40,27 @@ export class LazyLoadingMetadataService {
   }
 
   public initialiseAvailableSlides(lazyLoadingMetadata: LazyLoadingMetadata, reason: SlideInitialisation, duplicateImages: DuplicateImages, tag?: ImageTag, slideCount?: number): void {
-    this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:tag:", tag, "reason:", reason);
-    lazyLoadingMetadata.activeSlideIndex = 0;
-    lazyLoadingMetadata.selectedSlides = [];
-    const files: ContentMetadataItem[] = lazyLoadingMetadata?.contentMetadata?.files;
-    const imageTags: ImageTag[] = lazyLoadingMetadata?.contentMetadata?.imageTags;
-    if (tag === ALL_PHOTOS) {
-      lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.ALL);
-      this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", ALL_PHOTOS, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
-    } else if (tag === RECENT_PHOTOS) {
-      lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.RECENT);
-      this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", RECENT_PHOTOS, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
-    } else if (tag) {
-      lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.TAG, tag);
-      this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", tag.subject, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
-    } else {
-      lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.RECENT);
-      this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", reason, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
+    if (lazyLoadingMetadata) {
+      this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:tag:", tag, "reason:", reason);
+      lazyLoadingMetadata.activeSlideIndex = 0;
+      lazyLoadingMetadata.selectedSlides = [];
+      const files: ContentMetadataItem[] = lazyLoadingMetadata?.contentMetadata?.files;
+      const imageTags: ImageTag[] = lazyLoadingMetadata?.contentMetadata?.imageTags;
+      if (tag === ALL_PHOTOS) {
+        lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.ALL);
+        this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", ALL_PHOTOS, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
+      } else if (tag === RECENT_PHOTOS) {
+        lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.RECENT);
+        this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", RECENT_PHOTOS, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
+      } else if (tag) {
+        lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.TAG, tag);
+        this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", tag.subject, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
+      } else {
+        lazyLoadingMetadata.availableSlides = this.contentMetadataService.filterSlides(imageTags, files, duplicateImages, ImageFilterType.RECENT);
+        this.logger.info(lazyLoadingMetadata?.contentMetadata.name, "initialiseAvailableSlides:", reason, "selected:", this.stringUtils.pluraliseWithCount(lazyLoadingMetadata?.availableSlides.length, "image"));
+      }
+      this.add(lazyLoadingMetadata, slideCount, "add inside initialiseAvailableSlides");
     }
-    this.add(lazyLoadingMetadata, slideCount, "add inside initialiseAvailableSlides");
   }
 
   public add(lazyLoadingMetadata: LazyLoadingMetadata, slideCount?: number, reason?: string): ContentMetadataItem[] {

commit 3cf879961584d0932058ae4e756550e1258f7eb4

fix(mail+walks): protect open Brevo reads and fill contact-us merge fields (ref #317)
## What's new

Brevo admin read routes require a signed-in session again. They were left open when the forgot-password flow was moved to the backend (ref #157). Contact Us mail receives the same account address merge fields as other transactional mail. The walk map editor no longer holds unused mail and system config subscriptions.

## At a glance

- Brevo folders, contacts, lists, templates and account reads need a signed-in session
- Contact Us emails get street, town and postcode account merge fields
- Map edit drops unused mail and system config subscriptions

## Technical changes

- Restore `authConfig.authenticate()` on Brevo GET folders/contacts/lists/account and POST templates (regressed in 7a4cd7b8 while fixing forgot password, ref #157)
- `accountMergeFieldsFor()` on contact-us `resolve-and-send`
- Remove unused injects from `map-edit.ts`

projects/ngx-ramblers/src/app/pages/walks/walk-edit/map-edit.ts
@@ -10,7 +10,6 @@ import { Logger, LoggerFactory } from "../../../services/logger-factory.service"
 import { AlertInstance, NotifierService } from "../../../services/notifier.service";
 import { WalkDisplayService } from "../walk-display.service";
 import { StringUtilsService } from "../../../services/string-utils.service";
-import { MailMessagingService } from "../../../services/mail/mail-messaging.service";
 import { WalksConfigService } from "../../../services/system/walks-config.service";
 import { WalksConfig } from "../../../models/walks-config.model";
 import { AddressQueryService } from "../../../services/walks/address-query.service";
@@ -21,7 +20,6 @@ import { coerceBooleanProperty } from "@angular/cdk/coercion";
 import { sortBy } from "../../../functions/arrays";
 import { LeafletModule } from "@bluehalo/ngx-leaflet";
 import { MapTilesService } from "../../../services/maps/map-tiles.service";
-import { SystemConfigService } from "../../../services/system/system-config.service";
 import { MapMarkerStyleService } from "../../../services/maps/map-marker-style.service";
 import { GpxParserService } from "../../../services/maps/gpx-parser.service";
 import { UrlService } from "../../../services/url.service";
@@ -95,7 +93,6 @@ export class MapEditComponent implements OnInit, OnDestroy, OnChanges {
   private resizeObserver: ResizeObserver | null = null;
   private resizeFitTimer: ReturnType<typeof setTimeout> | null = null;
   private walksConfigService = inject(WalksConfigService);
-  private mailMessagingService = inject(MailMessagingService);
   private addressQueryService = inject(AddressQueryService);
   protected dateUtils = inject(DateUtilsService);
   public display = inject(WalkDisplayService);
@@ -103,7 +100,6 @@ export class MapEditComponent implements OnInit, OnDestroy, OnChanges {
   protected notifierService = inject(NotifierService);
   private logger: Logger = inject(LoggerFactory).createLogger("MapEditComponent", NgxLoggerLevel.ERROR);
   private zone = inject(NgZone);
-  private systemConfigService = inject(SystemConfigService);
   private mapTiles = inject(MapTilesService);
   private markerStyle = inject(MapMarkerStyleService);
   private gpxParser = inject(GpxParserService);
@@ -148,9 +144,6 @@ export class MapEditComponent implements OnInit, OnDestroy, OnChanges {
 
   private initializeSubscriptions() {
     this.subscriptions.push(
-      this.mailMessagingService.events().subscribe(config => {
-        this.logger.debug("MailMessagingConfig updated:", config);
-      }),
       this.walksConfigService.events().subscribe(config => {
         this.logger.info("WalksConfig updated:", config);
         this.walksConfig = config;

server/lib/brevo/brevo-routes.ts
@@ -58,9 +58,9 @@ import * as authConfig from "../auth/auth-config";
 
 const router = express.Router();
 
-router.get("/folders", folders);
-router.get("/contacts", contacts);
-router.get("/contacts/list", contactsInList);
+router.get("/folders", authConfig.authenticate(), folders);
+router.get("/contacts", authConfig.authenticate(), contacts);
+router.get("/contacts/list", authConfig.authenticate(), contactsInList);
 router.post("/contacts/delete", authConfig.authenticate(), contactsDelete);
 router.post("/contacts/create", authConfig.authenticate(), contactsCreate);
 router.post("/contacts/batch-update", authConfig.authenticate(), contactsBatchUpdate);
@@ -106,19 +106,19 @@ router.get("/campaign/queue", authConfig.authenticate(), queueSummaryRoute);
 router.post("/campaign/:campaignId/release", authConfig.authenticate(), releaseCampaignRoute);
 router.post("/campaign/:campaignId/cancel", authConfig.authenticate(), cancelCampaignRoute);
 router.get("/segments", authConfig.authenticate(), querySegments);
-router.post("/templates", queryTemplates);
+router.post("/templates", authConfig.authenticate(), queryTemplates);
 router.post("/templates/diff", authConfig.authenticate(), templateDiff);
 router.post("/templates/local-content", authConfig.authenticate(), localTemplateContent);
 router.get("/templates/local-names", authConfig.authenticate(), localTemplateNamesList);
 router.post("/templates/editable-body", authConfig.authenticate(), editableBodyContent);
 router.post("/templates/render", authConfig.authenticate(), renderTemplatePreview);
-router.get("/lists", lists);
+router.get("/lists", authConfig.authenticate(), lists);
 router.delete("/lists/delete", authConfig.authenticate(), listDelete);
 router.post("/lists/create", authConfig.authenticate(), listCreate);
 router.post("/lists/update", authConfig.authenticate(), listUpdate);
 router.post("/lists/subscriptions/export", authConfig.authenticate(), listSubscriptionsExport);
 router.post("/lists/subscriptions/parse", authConfig.authenticate(), receiveFileUpload, listSubscriptionsParse);
-router.get("/account", queryAccount);
+router.get("/account", authConfig.authenticate(), queryAccount);
 router.get("/domains", authConfig.authenticate(), domainsList);
 router.post("/domains/register", authConfig.authenticate(), registerDomainRoute);
 router.get("/domains/configuration", authConfig.authenticate(), domainConfigRoute);

server/lib/contact-us/resolve-and-send.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import debug from "debug";
 import { sendTransactionalMail } from "../brevo/transactional-mail/send-transactional-mail";
+import { accountMergeFieldsFor } from "../brevo/account/account";
 import * as config from "../mongo/controllers/config";
 import { ConfigKey } from "../../../projects/ngx-ramblers/src/app/models/config.model";
 import {
@@ -139,6 +140,9 @@ export async function sendContactUsTransactionalMail(req: Request, res: Response
     const roles: CommitteeMember[] = committeeCfg?.roles || [];
     const connectedEmails = new Set(await connectedInboxEmails(defaultTenantSlug()));
     emailRequest.to = await resolveContactRecipients(emailRequest.to || [], roles, connectedEmails);
+    if (emailRequest.params) {
+      emailRequest.params.accountMergeFields = await accountMergeFieldsFor();
+    }
     debugLog("sendContactUsTransactionalMail:resolved to:", emailRequest.to);
     return sendTransactionalMail(req, res, next);
   } catch (error) {